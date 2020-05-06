6b216fb27e6c1493b7392afa48d1d5879fe10bdf
kentaro84207 <kentaro84207@gmail.com>
2020-05-06 12:29:00 +0900

    Add canvas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from 'react-three-fiber';
+
+const Thing = () => {
+  const ref = useRef();
+  useFrame(() => (ref.current.rotation.z += 0.01));
+
+  return (
+    <mesh
+      ref={ref}
+      onClick={e => console.log('click')}
+      onPointerOver={e => console.log('hover')}
+      onPointerOut={e => console.log('unhover')}
+    >
+      <planeBufferGeometry attach='geometry' args={[1, 1]} />
+      <meshBasicMaterial
+        attach='material'
+        color='hotpink'
+        opacity={0.5}
+        transparent
+      />
+    </mesh>
+  )
+}
 
 const App = () => {
-  return <h1>test</h1>;
+  return (
+    <Canvas>
+      <Thing />
+    </Canvas>
+  )
 }
 
 export default App;
